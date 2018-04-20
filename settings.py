0b70c0b3b46922c2dc5719ffe3a7e2ce68e04a9b
Younes ALOUANI <alouani.younes@gmail.com>
2018-04-20 17:34:43 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Amazon variables. Be wary and don't hard-code your secret keys here. Rather,
+# set them as environment variables, or read them from a file somehow.
+AWS_CLIENT_SECRET_KEY = 'WDq/cy*****'
+AWS_SERVER_PUBLIC_KEY = 'AK*****'
+AWS_SERVER_SECRET_KEY = 'WDq/cy*****'
+
+AWS_EXPECTED_BUCKET = 'mybucketname'
+AWS_MAX_SIZE = 15000000
